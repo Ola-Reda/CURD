403cc4bdbc1ea10c0e7f2037386ca3e2ca735d17
OlaReda <ola.reda.900@gmail.com>
2025-11-13 10:28:32 +0200

    Finish it

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var productPrice = document.querySelector("#ProductPrice")
 var productCategory = document.querySelector("#ProductCategory")
 var productImage = document.querySelector("#ProductImage")
 var productDescription = document.querySelector("#ProductDes")
-
+var searchInput = document.querySelector("#SearchInput")
+var addBtn = document.querySelector("#AddBtn")
+var editBtn = document.querySelector("#EditBtn")
 
 var productsList = [];
 if(localStorage.getItem("products") !== null) {
@@ -17,7 +19,7 @@ function AddProducts() {
         price: productPrice.value,
         describtion: productDescription.value,
         category: productCategory.value,
-        image: ""
+        image: "samsung.jpg"
     }
     productsList.push(products)
     localStorage.setItem("products", JSON.stringify(productsList))
@@ -32,16 +34,16 @@ function displyProducts() {
     for(var i =0; i < productsList.length; i++){
         box += 
         `
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+            <div class="col-md-3">
+                <div class="card mb-3">
+                    <img src="${productsList[i].image}" class="card-img-top w-100" alt="...">
                     <div class="card-body">
                         <h3 class="card-title">${productsList[i].name}</h3>
                         <h5>${productsList[i].category}</h5>
                         <p><strong>price:</strong>${productsList[i].price}</p>
                         <p class="card-text">${productsList[i].describtion}</p>
-                        <button onclick="deleteProducts(${i})">delete</button>
-                        <button>update</button>
+                        <button onclick="deleteProducts(${i})" class="btn btn-outline-danger">delete <i class="fa-solid fa-trash"></i></button>
+                        <button class="btn btn-outline-warning" onclick="updateProductValue(${i})">update <i class="fa-solid fa-pencil"></i></button>
                     </div>
                 </div>
             </div>
@@ -50,11 +52,74 @@ function displyProducts() {
     }
 }
 
+var term;
+function updateProductValue(index) {
+    termIndex = index
+    productName.value = productsList[index].name
+    productPrice.value = productsList[index].price
+    productDescription.value = productsList[index].describtion
+    productCategory.value = productsList[index].category
+
+    addBtn.classList.add("d-none")
+    editBtn.classList.remove("d-none")
+
+}
+
+
+function updateProducts() {
+    productsList[termIndex].name = productName.value
+    productsList[termIndex].price = productPrice.value 
+    productsList[termIndex].describtion = productDescription.value 
+    productsList[termIndex].category = productCategory.value 
+    localStorage.setItem("products", JSON.stringify(productsList))
+    displyProducts()
+
+    addBtn.classList.remove("d-none")
+    editBtn.classList.add("d-none")
+
+}
+
 function deleteProducts(index) {
     productsList.splice(index,1)
     localStorage.setItem("products", JSON.stringify(productsList))
     displyProducts()
 }
+
+
+function searchProducts() {
+
+    var dataBox = document.querySelector(".data")
+
+var box = "";
+    for(var i =0; i < productsList.length; i++){
+        if((productsList[i].name).toLowerCase().includes((searchInput.value).toLowerCase())){
+             box += 
+        `
+            <div class="col-md-3">
+                <div class="card mb-3">
+                    <img src="${productsList[i].image}" class="card-img-top w-100" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">${productsList[i].name}</h3>
+                        <h5>${productsList[i].category}</h5>
+                        <p><strong>price:</strong>${productsList[i].price}</p>
+                        <p class="card-text">${productsList[i].describtion}</p>
+                        <button onclick="deleteProducts(${i})" class="btn btn-outline-danger">delete <i class="fa-solid fa-trash"></i></button>
+                        <button class="btn btn-outline-warning">update <i class="fa-solid fa-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+        `
+        }
+       
+    }
+    if(box == ``) {
+        box = "no products"
+        
+    }
+
+    dataBox.innerHTML = box
+}
+
 
 
 function clearInputsvalue() {
