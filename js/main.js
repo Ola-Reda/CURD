0f2bafbc3e086060da7625b8153e7c603c99f96e
OlaReda <ola.reda.900@gmail.com>
2025-11-18 12:01:10 +0200

    Add vaildation

--- js/main.js
+++ js/main.js
@@ -13,18 +13,102 @@ if(localStorage.getItem("products") !== null) {
     displyProducts()
 }
 
+
+function nameValidation() {
+    var regax = /^[a-zA-Z0-9]{3,15}$/
+    var myString = productName.value
+    if(regax.test(myString)) {
+        productName.classList.add("is-valid")
+        productName.classList.remove("is-invalid")
+        document.querySelector(".paratest").classList.add("d-none")
+        return true
+    }
+    else {
+        productName.classList.add("is-invalid")
+        productName.classList.remove("is-valid")
+        document.querySelector(".paratest").classList.remove("d-none")
+        return false
+    }
+}
+
+function priceValidation() {
+    var regax = /^[1-9][0-9]{0,6}$/
+    var myString = productPrice.value
+    if(regax.test(myString)) {
+        productPrice.classList.add("is-valid")
+        productPrice.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        productPrice.classList.add("is-invalid")
+        productPrice.classList.remove("is-valid")
+        return false
+    }
+}
+
+function categoryValidation() {
+    var regax = /^(tv|Mobile|Screens|Electornics)$/i
+    var myString = productCategory.value
+    console.log(regax.test(myString))
+    if(regax.test(myString)) {
+        productCategory.classList.add("is-valid")
+        productCategory.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        productCategory.classList.add("is-invalid")
+        productCategory.classList.remove("is-valid")
+        return false
+    }
+}
+
+function imageValidation() {
+    var regax = /^[\w-@ ]+\.(svg|png|jpg|jpeg)$/
+    var myString = productImage.value.slice(12)
+    console.log(regax.test(myString))
+    if(regax.test(myString)) {
+        productImage.classList.add("is-valid")
+        productImage.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        productImage.classList.add("is-invalid")
+        productImage.classList.remove("is-valid")
+        return false
+    }
+}
+
+function descValidation() {
+    var regax = /^.+$/
+    var myString = productDescription.value
+    console.log(regax.test(myString))
+    if(regax.test(myString)) {
+        productDescription.classList.add("is-valid")
+        productDescription.classList.remove("is-invalid")
+        return true
+    }
+    else {
+        productDescription.classList.add("is-invalid")
+        productDescription.classList.remove("is-valid")
+        return false
+    }
+}
+
+
 function AddProducts() {
-    var products = {
+    if(nameValidation() && priceValidation() && categoryValidation() && imageValidation() && descValidation() ){
+        var products = {
         name: productName.value,
         price: productPrice.value,
         describtion: productDescription.value,
         category: productCategory.value,
         image: `imgs/${productImage.value.slice(12)}`
+        }
+        productsList.push(products)
+        localStorage.setItem("products", JSON.stringify(productsList))
+        displyProducts();
+        clearInputsvalue()
     }
-    productsList.push(products)
-    localStorage.setItem("products", JSON.stringify(productsList))
-    displyProducts();
-    clearInputsvalue()
 }
 
 function displyProducts() {
@@ -138,4 +222,9 @@ function clearInputsvalue() {
     productPrice.value = ""
     productDescription.value = ''
     productCategory.value = ""
+    productName.classList.remove("is-valid")
+    productPrice.classList.remove("is-valid")
+    productCategory.classList.remove("is-valid")
+    productImage.classList.remove("is-valid")
+    productDescription.classList.remove("is-valid")
 }
